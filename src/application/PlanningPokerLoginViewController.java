678227526c933220a74dc664c56e2c7e79e32897
iamjasonlobo <jasonlobo.official@gmail.com>
2023-11-28 06:08:47 -0700

    Connected Planning Poker Login to DB

--- src/application/PlanningPokerLoginViewController.java
+++ src/application/PlanningPokerLoginViewController.java
@@ -4,116 +4,64 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.Node;
 import javafx.stage.Stage;
-//import javafx.collections.FXCollections;
-//import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import java.io.IOException;
-import java.io.FileOutputStream;
-//import java.net.URL;
-//import java.util.ResourceBundle;
-//import javafx.fxml.Initializable;
-import javafx.scene.control.TextField;
-import java.io.*; 
-//import java.util.*; 
+
 public class PlanningPokerLoginViewController extends Controller {
-	private Scene scene;
-	private Stage stage;
-	//private Login login = new Login();
-	private SessionList sessionList;
-	
-	
-	@FXML
-	private TextField sessionNameField;
-	@FXML
-	private PasswordField passwordField;
-	@FXML 
-	private Label errorLabel;
+    private Scene scene;
+    private Stage stage;
+    private SessionDAO sessionDAO = new SessionDAO();
 
-	
-	
+    @FXML
+    private TextField sessionNameField;
+    @FXML
+    private PasswordField passwordField;
+    @FXML 
+    private Label errorLabel;
 
-	public void writeToFile(byte[] serializedSessions) {
-        try (FileOutputStream fos = new FileOutputStream("session" + ".ser")) {
-            fos.write(serializedSessions);
-        } catch (IOException e) {
-            e.printStackTrace();
+    @FXML
+    protected void onCreateSessionPressed() {
+        String sessionName = sessionNameField.getText();
+        String pin = passwordField.getText();
+
+        if (!pin.matches("\\d{5}")) {
+            errorLabel.setText("PIN must be exactly 5 digits and contain no special characters or letters!");
+            return;
         }
-	}
-	
-	@SuppressWarnings("resource")
-	public SessionList readFromFile() {
-		try{
-			FileInputStream streamIn = new FileInputStream("session.ser");
-			ObjectInputStream ois = new ObjectInputStream(streamIn);
-			SessionList newList = (SessionList) ois.readObject();
-			return newList;
-		} catch (IOException | ClassNotFoundException e) {
-			return null;
-			//e.printStackTrace();
-            //return null;
+
+        Session newSession = new Session(sessionName, pin);
+        if(sessionDAO.findSessionByName(sessionName) == null) {
+            sessionDAO.addSession(newSession);
+            errorLabel.setText("Session successfully added!");
         }
-	}
-	
-	@FXML
-	protected void onCreateSessionPressed() {
-	    String sessionName = sessionNameField.getText();
-	    String password = passwordField.getText();
-	    sessionList = null;
-	    sessionList = readFromFile();
-	    if(sessionList == null) {
-	        sessionList = new SessionList();
-	    }
+        else {
+            errorLabel.setText("This session already exists!");
+            return;
+        }
+    }
 
-	    if (!password.matches("\\d{5}")) {
-	        errorLabel.setText("PIN must be exactly 5 digits and contain no special characters or letters!");
-	        return;
-	    }
+    public void submitLogin(ActionEvent event) throws IOException {
+        String sessionName = sessionNameField.getText();
+        String pin = passwordField.getText();
+        Session currentSession = sessionDAO.findSessionByName(sessionName);
 
-	    Session newSession = new Session(sessionName, password);
-	    if(sessionList.findSession(sessionName) == null) {
-	        sessionList.addSession(newSession);
-	        errorLabel.setText("Session successfully added!");
-	    }
-	    else {
-	        errorLabel.setText("This session already exists!");
-	        return;
-	    }
-	    byte[] serializedSessions = sessionList.serialize();
-	    writeToFile(serializedSessions);
-	}
+        if(currentSession == null) {
+            errorLabel.setText("This session does not exist");
+            return;
+        }
 
-	public void submitLogin(ActionEvent event) throws IOException
-	{
-		String sessionName = sessionNameField.getText();
-		String password = passwordField.getText();
-		sessionList = readFromFile();
-		if(sessionList == null) {
-			errorLabel.setText("This session does not exist");
-			return;
-		}
-		Session currentSession = sessionList.findSession(sessionName);
-		if(currentSession == null) {
-			errorLabel.setText("This session does not exist");
-			return;
-		}
-		
-		if(currentSession.getPassword().equals(password)) {
-//			Main.currentSession = currentSession;
-			//initializeFields();
-			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("PlanningPokerPage.fxml"));
-			stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-			scene = new Scene(fxmlLoader.load(), 600, 400);
-			stage.setTitle("Login Info");
-			stage.setScene(scene);
-			stage.show();
-		}
-		else {
-			errorLabel.setText("Incorrect PIN");
-		}
-		
-	}
-	
-
+        if(currentSession.getPin().equals(pin)) {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("PlanningPokerPage.fxml"));
+            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            scene = new Scene(fxmlLoader.load(), 600, 400);
+            stage.setTitle("Login Info");
+            stage.setScene(scene);
+            stage.show();
+        }
+        else {
+            errorLabel.setText("Incorrect PIN");
+        }
+    }
 }
---
+++ src/application/Session_LEGACY.java
@@ -0,0 +1,101 @@
+package application;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Session_LEGACY implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String sessionName;
+	private String password;
+	private ArrayList<Project> projects;
+	
+	public Session_LEGACY(String sessionName, String password) {
+		this.sessionName = sessionName;
+		this.password = password;
+		projects = new ArrayList<Project>();
+	}
+	
+	public String getSessionName() {
+		return this.sessionName;
+	}
+	
+	public String getPassword() {
+		return this.password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public Project getProject(String projectName) {
+		for(int i =0; i<projects.size();i++){
+			if(projects.get(i).getName().equals(projectName)) {
+				return projects.get(i);
+			}
+		}
+		return null;
+		
+	}
+	
+	public ArrayList<Project> getProjects(){
+		return this.projects;
+	}
+	
+	public void createProject(String projectName) {
+		Project newProject = new Project(projectName);
+		projects.add(newProject);
+	}
+	
+	public Project getSingleProject(String projectName) {
+		for(int i = 0; i < projects.size(); i++) {
+			if(projects.get(i).getName().equals(projectName)) {
+				return projects.get(i);
+			}
+		}
+		return null;
+	}
+	public List<String> getProjectNames(){
+		List<String> names = new ArrayList<String>();
+		for(int i =0; i< projects.size();i++) {
+			names.add(projects.get(i).getName());
+		}
+		return names;
+	}
+	
+	
+	public void printProjectNames(){
+		for(int i =0; i< projects.size();i++) {
+			System.out.println(projects.get(i).getName());
+		}
+	}
+	
+	
+	public byte[] serialize() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        try (ObjectOutputStream oos = new ObjectOutputStream(out)) {
+            oos.writeObject(this);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return out.toByteArray();
+    }
+	
+	public static Session_LEGACY deserialize(byte[] data) {
+        ByteArrayInputStream in = new ByteArrayInputStream(data);
+        Session_LEGACY session = null;
+        try (ObjectInputStream ois = new ObjectInputStream(in)) {
+            session = (Session_LEGACY) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return session;
+    }
+}
